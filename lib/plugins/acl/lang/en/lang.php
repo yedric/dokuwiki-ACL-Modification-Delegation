8fd7dae12983b7b1250a6534c74511edf805979f
Ed Pate <github.com@jaxcon.net>
2011-06-15 10:33:45 -0700

    ACLMOD v1.1

--- lib/plugins/acl/lang/en/lang.php
+++ lib/plugins/acl/lang/en/lang.php
@@ -6,6 +6,7 @@
  * @author     Andreas Gohr <andi@splitbrain.org>
  * @author     Anika Henke <anika@selfthinker.org>
  * @author     Matthias Grimm <matthiasgrimm@users.sourceforge.net>
+ * @author     Ed Pate <dokuwikid@jaxcon.net>
  */
 
 $lang['admin_acl']  = 'Access Control List Management';
@@ -28,7 +29,10 @@ $lang['p_choose_ns']  = 'Please <b>enter a user or group</b> in the form above t
 
 $lang['p_inherited']  = 'Note: Those permissions were not set explicitly but were inherited from other groups or higher namespaces.';
 $lang['p_isadmin']    = 'Note: The selected group or user has always full permissions because it is configured as superuser.';
-$lang['p_include']    = 'Higher permissions include lower ones. Create, Upload and Delete permissions only apply to namespaces, not pages.';
+// line modified for ACLMOD v1.1
+$lang['p_include']    = 'Higher permissions include lower ones. Create, Upload, Delete, and ACLMOD permissions only apply to namespaces, not pages.';
+// line added for ACLMOD v1.1
+$lang['p_denied']     = 'Only admin users may modify ACLs with ACLmod permissions set.';
 
 $lang['current'] = 'Current ACL Rules';
 $lang['where'] = 'Page/Namespace';
@@ -41,6 +45,9 @@ $lang['acl_perm2']  = 'Edit';
 $lang['acl_perm4']  = 'Create';
 $lang['acl_perm8']  = 'Upload';
 $lang['acl_perm16'] = 'Delete';
+// line added for ACLMOD v1.1
+$lang['acl_perm64'] = 'ACLMOD';
 $lang['acl_new']    = 'Add new Entry';
 $lang['acl_mod']    = 'Modify Entry';
 //Setup VIM: ex: et ts=2 enc=utf-8 :
+
