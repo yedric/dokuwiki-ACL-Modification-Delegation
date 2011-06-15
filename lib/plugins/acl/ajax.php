c2ac5a9ffbdaf3599157e5a702ad6d1de1a2fefd
Ed Pate <github.com@jaxcon.net>
2011-06-15 10:32:23 -0700

    ACLMOD v1.1

--- lib/plugins/acl/ajax.php
+++ lib/plugins/acl/ajax.php
@@ -4,6 +4,7 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Andreas Gohr <andi@splitbrain.org>
+ * @author     Ed Pate <dokuwikid@jaxcon.net>
  */
 
 //fix for Opera XMLHttpRequests
@@ -16,7 +17,32 @@ require_once(DOKU_INC.'inc/init.php');
 //close session
 session_write_close();
 
-if(!auth_isadmin()) die('for admins only');
+// *Begin* ACLMOD v1.1 changes
+if
+    (
+        !$INFO['isadmin']
+        &&
+        (
+            ($_REQUEST['id'] != '')
+            &&
+            (
+                (auth_quicknsaclcheck($_REQUEST['id']) < AUTH_ACLMOD )
+                ||
+                (auth_quicknsaclcheck($_REQUEST['id'].':') <AUTH_ACLMOD )
+            )
+        )
+        &&
+        (
+            ( $_REQUEST['ns'] != '')
+            &&
+            (
+                (auth_quicknsaclcheck($_REQUEST['ns']) < AUTH_ACLMOD )
+                ||
+                (auth_quicknsaclcheck($_REQUEST['ns'].':') <AUTH_ACLMOD )
+            )
+        )
+    ) die('for admins only--');
+// **End** ACLMOD v1.1 changes
 if(!checkSecurityToken()) die('CRSF Attack');
 
 $ID    = getID();
@@ -32,6 +58,8 @@ if($ajax == 'info'){
 }elseif($ajax == 'tree'){
     global $conf;
     global $ID;
+// line added for ACLMOD v1.1
+    global $INFO;
 
     $dir = $conf['datadir'];
     $ns  = $_REQUEST['ns'];
@@ -44,13 +72,18 @@ if($ajax == 'info'){
 
     $data = $acl->_get_tree($ns,$ns);
 
+// *Begin* ACLMOD v1.1 changes
     foreach($data as $item){
-        $item['level'] = $lvl+1;
-        echo $acl->_html_li_acl($item);
-        echo '<div class="li">';
-        echo $acl->_html_list_acl($item);
-        echo '</div>';
-        echo '</li>';
+            f ( ( $item['type'] == 'd' && ( auth_quicknsaclcheck($item['id'].':') >= AUTH_ACLMOD ) ) || ( $item['type'] == 'f' && ( auth_quicknsaclcheck($item['id']) >= AUTH_A
+CLMOD ) )  ) {
+            $item['level'] = $lvl+1;
+            echo $acl->_html_li_acl($item);
+            echo '<div class="li">';
+            echo $acl->_html_list_acl($item);
+            echo '</div>';
+            echo '</li>';
+// **End** ACLMOD v1.1 changes
     }
 }
+
 
