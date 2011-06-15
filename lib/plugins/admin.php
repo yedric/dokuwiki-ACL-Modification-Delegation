58874fabc0b8b7b2cd081f4df8b14a531e60097d
Ed Pate <github.com@jaxcon.net>
2011-06-15 10:28:52 -0700

    ACLMOD v1.1

--- lib/plugins/admin.php
+++ lib/plugins/admin.php
@@ -4,6 +4,7 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Christopher Smith <chris@jalakai.co.uk>
+ * @author     Ed Pate <dokuwikid@jaxcon.net>
  */
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
@@ -35,12 +36,19 @@ class DokuWiki_Admin_Plugin extends DokuWiki_Plugin {
         trigger_error('html() not implemented in '.get_class($this), E_USER_WARNING);
     }
 
+// *Begin* ACLMOD v1.1 changes
     function forAdminOnly() {
-        return true;
+        if ($INFO['nsperm'] >= AUTH_ACLMOD) {
+            return false;
+        } else {
+            return true;
+        }
     }
+// **End** ACLMOD v1.1 changes
 
     function getTOC(){
         return array();
     }
 }
 //Setup VIM: ex: et ts=4 enc=utf-8 :
+
